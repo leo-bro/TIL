e3017b4ba836c8d86d441df494a1ed416d98a7c9
Tae Hun Jung <taehun.jung15@gmail.com>
2025-01-21 20:32:22 +0900

    Delete dummy files

--- git/test.py
+++
@@ -1,9 +0,0 @@
-def solution(a, d, included):
-    answer = 0
-    n = len(included)
-    numarr = [x for x in range(a, a + (n - 1) * d, d)]
-    answer = sum([numarr[i] * int(included[i]) for i in range(n)])
-    return answer
-
-
-solution(3, 4, [True, False, False, True, True])
--- python/mousetracker.py
+++
@@ -1,8 +0,0 @@
-from pynput.mouse import Listener
-
-def on_move(x, y):
-    print(f"Pointer moved to ({x}, {y})")
-
-# 마우스 움직임을 감지하는 리스너를 설정합니다.
-with Listener(on_move=on_move) as listener:
-    listener.join()
