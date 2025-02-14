d56d2c55b3d455d7ebe7041dce816600e5c442ee
Tae Hun Jung <taehun.jung15@gmail.com>
2025-02-14 18:46:15 +0900

    update ml-timeseries.py
    fix with copilot

--- python/ml-timeseries/ml-timeseries.py
+++ python/ml-timeseries/ml-timeseries.py
@@ -1,36 +1,48 @@
-# ML approach for time series data
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-### data creation
-
+# 데이터 생성
 N = 600
 
-t = np.arange(0, N, 1).reshape(-1,1)
-t = np.array([t[i] + np.random.rand(1)/4 for i in range(len(t))])
-t = np.array([t[i] - np.random.rand(1)/7 for i in range(len(t))])
+t = np.arange(0, N, 1).reshape(-1, 1)
+t = np.array([t[i] + np.random.rand(1) / 4 for i in range(len(t))])
+t = np.array([t[i] - np.random.rand(1) / 7 for i in range(len(t))])
 t = np.array(np.round(t, 2))
 
-x1 = np.round((np.random.random(N) * 5).reshape(-1,1), 2)
-x2 = np.round((np.random.random(N) * 5).reshape(-1,1), 2)
-x3 = np.round((np.random.random(N) * 5).reshape(-1,1), 2)
+x1 = np.round((np.random.random(N) * 5).reshape(-1, 1), 2)
+x2 = np.round((np.random.random(N) * 5).reshape(-1, 1), 2)
+x3 = np.round((np.random.random(N) * 5).reshape(-1, 1), 2)
 
-n = np.round((np.random.random(N) * 2).reshape(-1,1), 2)
+n = np.round((np.random.random(N) * 2).reshape(-1, 1), 2)
 
-y = np.array([((np.log(np.abs(2 + x1[t])) - x2[t-1]**2) + 0.02*x3[t-3]*np.exp(x1[t-1])) for t in range(len(t))])
-y = np.round(y+n, 2)
+y = np.array(
+    [
+        (
+            (np.log(np.abs(2 + x1[t])) - x2[t - 1] ** 2)
+            + 0.02 * x3[t - 3] * np.exp(x1[t - 1])
+        )
+        for t in range(len(t))
+    ]
+)
+y = np.round(y + n, 2)
 
-plt.plot(t,y)
+# 데이터 시각화
+plt.plot(t, y)
+plt.xlabel("Time")
+plt.ylabel("Value")
+plt.title("Time Series Data")
 plt.show()
 
-### framing window
+# 데이터 프레임 생성 및 전처리
+dataset = pd.DataFrame(
+    np.concatenate((t, x1, x2, x3, y), axis=1), columns=["t", "x1", "x2", "x3", "y"]
+)
 
-dataset = pd.DataFrame(np.concatenate((t, x1, x2, x3, y), axis=1), 
-                       columns=['t', 'x1', 'x2', 'x3', 'y'])
-
-deltaT = np.array([(dataset.t[i + 1] - dataset.t[i]) for i in range(len(dataset)-1)])
+deltaT = np.array([(dataset.t[i + 1] - dataset.t[i]) for i in range(len(dataset) - 1)])
 deltaT = np.concatenate((np.array([0]), deltaT))
 
-dataset.insert(1, '∆t', deltaT)
-dataset.head(3)
+dataset.insert(1, "∆t", deltaT)
+
+# 데이터 확인
+print(dataset.head(3))
