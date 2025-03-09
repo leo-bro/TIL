59707cf778f605f7a3d065064fb8e13dc403a2a4
Tae Hun Jung <taehun.jung15@gmail.com>
2025-03-09 22:44:31 +0900

    Add new coding test example
    mfv.py

---
+++ python/programmers/mfv.py
@@ -0,0 +1,23 @@
+def solution(array):
+    answer = []
+    num = list(set(array))
+    cnt = [0] * len(set(array))
+    for i, n in enumerate(num):
+        for a in array:
+            if n == a:
+                cnt[i] += 1
+    m = max(cnt)
+    for i, c in enumerate(cnt):
+        if c == m:
+            answer += [num[i]]
+    answer = -1 if len(answer) > 1 else answer[0]
+    return answer
+
+
+def solution(array):
+    while len(array) != 0:
+        for i, a in enumerate(set(array)):
+            array.remove(a)
+        if i == 0:
+            return a
+    return -1
